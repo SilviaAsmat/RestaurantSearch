4bc46934987b0e704f680c4df353acbd35eaac04
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2025-10-23 14:13:02 -0500

    removed extra base url

--- restaurant-discovery-frontend/src/api/client.js
+++ restaurant-discovery-frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://127.0.0.1:3000/",
+  baseURL: process.env.REACT_APP_API_URL,
   timeout: 10000, // 10s
 });
 
